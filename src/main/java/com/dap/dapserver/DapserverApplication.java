b5dd7ea43b45edff10891ef8ace73b39a35824f2
“wutingting” <1269878284@qq.com>
2019-06-20 23:45:52 +0800

    初始化spring boot项目

---
+++ src/main/java/com/dap/dapserver/DapserverApplication.java
@@ -0,0 +1,13 @@
+package com.dap.dapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapserverApplication.class, args);
+	}
+
+}
